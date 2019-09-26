1b30c87a9d0940f1131dcbca5a2d33a439ef7f12
samlaiw <samyes89@gmail.com>
2019-09-27 05:22:18 +0800

    demo

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+    // $success = true;
+    // $error = "";
+    // if($_POST["account"] == 123){
+    //     $success = false;
+    //     $error = "帳號已使用";
+    // }
+    // if($_POST["password"] == 123){
+    //     $success = false;
+    //     $error = "密碼格式不符";
+    // }
+
+    // $json = array(
+    //     "success" => $success,
+    //     "error" => $error
+    // );
+    
+    // echo json_encode($json);
+    print_r($_FILES);
+    print_r($_POST);
+?>
